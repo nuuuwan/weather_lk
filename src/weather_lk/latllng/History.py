90da935f0f7bda35e6308ef5b2eec75a0458c2fa
nuuuwan <nuuuwan@gmail.com>
2023-09-01 06:46:57 +0530

    Added History

---
+++ src/weather_lk/latllng/History.py
@@ -0,0 +1,81 @@
+import os
+import tempfile
+from functools import cached_property
+
+import googlemaps
+from utils import Git, JSONFile, Log
+
+log = Log('History')
+
+GIT_REPO_URL = 'https://github.com/nuuuwan/weather_lk'
+DIR_REPO = os.path.join(tempfile.gettempdir(), 'weather_lk')
+BRANCH_NAME = 'data'
+PLACE_TO_LATLNG_PATH = os.path.join('data', 'place_to_latlng.json')
+PLACE_TO_LATLNG_PATH_NEW = os.path.join('data', 'place_to_latlng.new.json')
+
+GMAPS = googlemaps.Client(os.environ['GMAPS_API_KEY'])
+DEFAULT_LATLNG = [0, 0]
+
+
+class History:
+    @cached_property
+    def history_list(self) -> list:
+        self.git = Git(GIT_REPO_URL)
+        self.git.clone(DIR_REPO)
+        self.git.checkout(BRANCH_NAME)
+
+        history_list = []
+        for file_only in os.listdir(DIR_REPO):
+            if not (
+                file_only.startswith('weather_lk.')
+                and file_only.endswith('.json')
+            ):
+                continue
+            file_path = os.path.join(DIR_REPO, file_only)
+            data = JSONFile(file_path).read()
+            date = data['date']
+            weather_list = data['weather_list']
+            history_list.append(dict(date=date, weather_list=weather_list))
+        n = len(history_list)
+        log.info(f'Loaded data for {n} days.')
+        return history_list
+
+    def get_place_to_latlng(self, place_to_latlng_old) -> dict:
+        place_to_latlng = {}
+        for history in self.history_list:
+            for weather in history['weather_list']:
+                place = weather['place']
+                if place not in place_to_latlng:
+                    if (
+                        place_to_latlng_old.get(place, DEFAULT_LATLNG)
+                        != DEFAULT_LATLNG
+                    ):
+                        place_to_latlng[place] = place_to_latlng_old[place]
+                    else:
+                        latlng = History.get_latlng(place)
+                        log.debug(f'{place} -> {latlng}')
+                        place_to_latlng[place] = latlng
+        place_to_latlng = dict(
+            sorted(place_to_latlng.items(), key=lambda item: item[0])
+        )
+        return place_to_latlng
+
+    @staticmethod
+    def get_latlng(place: str):
+        geocode_result = GMAPS.geocode(f'{place}, Sri Lanka')
+        if not geocode_result:
+            return DEFAULT_LATLNG
+        d = geocode_result[0]['geometry']['location']
+        return (d['lat'], d['lng'])
+
+    def save_place_to_latlng(self):
+        place_to_latlng_old = JSONFile(PLACE_TO_LATLNG_PATH).read()
+        place_to_latlng = self.get_place_to_latlng(place_to_latlng_old)
+
+        n = len(place_to_latlng.keys())
+        JSONFile(PLACE_TO_LATLNG_PATH_NEW).write(place_to_latlng)
+        log.info(f'Saved {n} places to {PLACE_TO_LATLNG_PATH}.')
+
+
+if __name__ == '__main__':
+    History().save_place_to_latlng()
