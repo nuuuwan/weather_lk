a4ac906cf1cae13af4db9351b695e8ea6ad0eddc
nuuuwan <nuuuwan@gmail.com>
2024-02-23 12:05:08 +0530

    simplified min max functions

--- src/weather_lk/meteo_gov_lk/PDFParserExpandedData.py
+++ src/weather_lk/meteo_gov_lk/PDFParserExpandedData.py
@@ -46,64 +46,52 @@ class PDFParserExpandedData:
         return expanded_weather_list
 
     @cached_property
-    def min_temp(self):
-        min_temp = None
-        min_temp_place = None
-
-        weather_list = self.weather_list
-        for weather in weather_list:
-            place = weather.get('place')
-            _min_temp = weather.get('min_temp')
-
-            if _min_temp is not None and (
-                min_temp is None or min_temp > _min_temp
-            ):
-                min_temp = _min_temp
-                min_temp_place = place
-
-        return dict(
-            min_temp=min_temp,
-            min_temp_place=min_temp_place,
-        )
-
-    @cached_property
-    def max_temp(self):
-        max_temp = None
-        max_temp_place = None
-
-        weather_list = self.weather_list
-        for weather in weather_list:
-            place = weather.get('place')
-            _max_temp = weather.get('max_temp')
-
-            if _max_temp is not None and (
-                max_temp is None or max_temp < _max_temp
-            ):
-                max_temp = _max_temp
-                max_temp_place = place
-
-        return dict(
-            max_temp=max_temp,
-            max_temp_place=max_temp_place,
-        )
-
-    @cached_property
     def max_rain(self):
-        max_rain = None
-        max_rain_place = None
-
-        weather_list = self.weather_list
-        for weather in weather_list:
-            place = weather.get('place')
-            rain = weather.get('rain')
-
-            if rain is not None and (max_rain is None or max_rain < rain):
-                max_rain = rain
-                max_rain_place = place
+        place_and_max = [
+            [
+                weather.get('place'),
+                weather.get('rain'),
+            ]
+            for weather in self.weather_list
+        ]
+        sorted_max = sorted(place_and_max, key=lambda x: x[1], reverse=True)
+        max_rain = sorted_max[0][2]
+        max_rain_place = sorted_max[0][0]
 
         return dict(
             max_rain=max_rain,
             max_rain_place=max_rain_place,
+        )
+
+    @cached_property
+    def min_max_temp(self):
+        place_min_and_max = [
+            [
+                weather.get('place'),
+                weather.get('min_temp'),
+                weather.get('max_temp'),
+            ]
+            for weather in self.weather_list
+        ]
+        sorted_min = sorted(place_min_and_max, key=lambda x: x[1])
+        min_temp = sorted_min[0][1]
+        min_temp_place = sorted_min[0][0]
+
+        sorted_max = sorted(
+            place_min_and_max, key=lambda x: x[2], reverse=True
+        )
+        max_temp = sorted_max[0][2]
+        max_temp_place = sorted_max[0][0]
+
+        return dict(
+            min_temp=dict(
+                min_temp=min_temp,
+                min_temp_place=min_temp_place,
+            ),
+            max_temp=dict(
+                max_temp=max_temp,
+                max_temp_place=max_temp_place,
+            ),
         )
 
     @cached_property
@@ -112,14 +100,16 @@ class PDFParserExpandedData:
         date = TIME_FORMAT_DATE.stringify(Time(date_ut))
         log.debug(f'{date=}')
 
-        expanded_data = dict(
-            date_ut=date_ut,
-            date=date,
-            min_temp=self.min_temp,
-            max_temp=self.max_temp,
-            max_rain=self.max_rain,
-            weather_list=self.weather_list,
+        expanded_data = (
+            dict(
+                date_ut=date_ut,
+                date=date,
+                weather_list=self.weather_list,
+                max_rain=self.max_rain,
+            )
+            | self.min_max_temp
         )
+
         return expanded_data
 
     def write_json(self):
