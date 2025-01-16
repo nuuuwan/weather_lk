348e9bc6e4a31e830c7a9cf21c4a466ff5b93cfb
nuuuwan <nuuuwan@gmail.com>
2025-01-16 11:40:07 +0530

    T_WAIT = 30

--- src/weather_lk/meteo_gov_lk/MeteoGovLkPage.py
+++ src/weather_lk/meteo_gov_lk/MeteoGovLkPage.py
@@ -8,13 +8,13 @@ from utils import Log
 from utils_future import RemotePDF
 from weather_lk.core import Data
 
-log = Log('weather_lk')
+log = Log("weather_lk")
 
 
 class MeteoGovLkPage:
-    URL = 'http://meteo.gov.lk/index.php?lang=en'
+    URL = "http://meteo.gov.lk/index.php?lang=en"
     PAGE_LOAD_TIMEOUT = 240
-    T_WAIT = 4
+    T_WAIT = 30
 
     @cached_property
     def pdf_url(self):
@@ -23,16 +23,16 @@ class MeteoGovLkPage:
         browser = webdriver.Firefox(options=options)
         browser.set_page_load_timeout(MeteoGovLkPage.PAGE_LOAD_TIMEOUT)
 
-        log.debug(f'Browsing {MeteoGovLkPage.URL}...')
+        log.debug(f"Browsing {MeteoGovLkPage.URL}...")
         browser.get(self.URL)
-        log.debug(f'😴 Sleeping for {MeteoGovLkPage.T_WAIT}s...')
+        log.debug(f"😴 Sleeping for {MeteoGovLkPage.T_WAIT}s...")
         time.sleep(MeteoGovLkPage.T_WAIT)
 
         a_daily = browser.find_element(
             "xpath", "//a[text()='Daily Weather Summary']"
         )
-        pdf_url = a_daily.get_attribute('href')
-        log.debug(f'{pdf_url=}')
+        pdf_url = a_daily.get_attribute("href")
+        log.debug(f"{pdf_url=}")
         browser.quit()
         return pdf_url
 
