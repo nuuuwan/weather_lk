437a87adb1fa28b908aef03ebe966f20c6dbb621
nuuuwan <nuuuwan@gmail.com>
2025-09-07 09:37:52 +0530

    Deleted unused

--- src/weather_lk/meteo_gov_lk/MeteoGovLkPage.py
+++ src/weather_lk/meteo_gov_lk/MeteoGovLkPage.py
@@ -4,8 +4,6 @@ from functools import cached_property
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from utils import Log
 
 from utils_future import RemotePDF
