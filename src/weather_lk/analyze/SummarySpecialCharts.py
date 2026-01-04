1dd8d4abf07384a093e47c376fbeafb8e0ab6877
nuuuwan <nuuuwan@gmail.com>
2026-01-04 08:46:19 +0530

    Refactor SummarySpecialCharts.py for consistent use of double quotes and improved readability

--- src/weather_lk/analyze/SummarySpecialCharts.py
+++ src/weather_lk/analyze/SummarySpecialCharts.py
@@ -9,11 +9,11 @@ from weather_lk.charts.ChartMinMaxPlot import ChartMinMaxPlot
 from weather_lk.constants import DISPLAY_PLACES, TEST_MODE
 from weather_lk.core.Data import Data
 
-log = Log('SummarySpecialCharts')
+log = Log("SummarySpecialCharts")
 
 
 class SummarySpecialCharts:
-    README_MIN_MAX_PLOT_PATH = 'README.min_max_plot.md'
+    README_MIN_MAX_PLOT_PATH = "README.min_max_plot.md"
 
     @staticmethod
     def init():
@@ -27,39 +27,39 @@ class SummarySpecialCharts:
         idx_by_place = Data.idx_by_place()
         readme_lines = []
         for place in DISPLAY_PLACES:
-            if TEST_MODE and place != 'Colombo':
+            if TEST_MODE and place != "Colombo":
                 continue
             data_for_place = idx_by_place.get(place, None)
             if data_for_place is None:
-                log.warning(f'No data for {place}')
+                log.warning(f"No data for {place}")
                 continue
 
             ChartMinMaxPlot(place, data_for_place).write()
             label = SummaryWriteDataByPlace.get_place_label(place)
             image_path_rain = (
                 SummaryReadMe.URL_REMOTE_DATA
-                + '/charts/min_max_plot/'
-                + f'{label}.png'
+                + "/charts/min_max_plot/"
+                + f"{label}.png"
             )
             readme_lines.extend(
                 [
-                    f'### {place}',
-                    '',
-                    f'![{place} Min Max Plot]({image_path_rain})',
-                    '',
-                    '',
+                    f"### {place}",
+                    "",
+                    f"![{place} Min Max Plot]({image_path_rain})",
+                    "",
+                    "",
                 ]
             )
         return readme_lines
 
     def build_min_max_plot_readme(self, lines_inner):
-        lines = ['# Min Max Plots', ''] + lines_inner
+        lines = ["# Min Max Plots", ""] + lines_inner
 
         readme_path = os.path.join(
             Data.DIR_REPO, SummarySpecialCharts.README_MIN_MAX_PLOT_PATH
         )
         File(readme_path).write_lines(lines)
-        log.info(f'Wrote {readme_path}')
+        log.info(f"Wrote {readme_path}")
 
     def build_min_max_plot(self):
         lines_inner = self.draw_min_max_plot()
@@ -71,8 +71,9 @@ class SummarySpecialCharts:
     @cached_property
     def lines_special_charts(self):
         return [
-            '',
-            '## Special Charts',
-            '',
-            f'* [Min Max Plot]({SummarySpecialCharts.README_MIN_MAX_PLOT_PATH})',
+            "",
+            "## Special Charts",
+            "",
+            "* [Min Max Plot]"
+            + f"({SummarySpecialCharts.README_MIN_MAX_PLOT_PATH})",
         ]
